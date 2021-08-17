8b318c09076b6fc1be810a258dea8e7b58e4b897
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-17 20:34:21 +0200

    ConstructBinTrFromPostAndInorder

--- src/ConstructBinTrFromPostAndInorder.java
+++ src/ConstructBinTrFromPostAndInorder.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class ConstructBinTrFromPostAndInorder {
+    // time o(n), space O(n)
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         HashMap<Integer, Integer> inorMap = new HashMap<>();
         for(int i = 0; i < inorder.length; i++){
@@ -16,7 +17,9 @@ public class ConstructBinTrFromPostAndInorder {
         int rootVal = postorder[postIdx];
         TreeNode newRoot = new TreeNode(rootVal);
         int inorderIdx = inorMap.get(rootVal);
-
+        // to extract the index of the left subtree's root from the postorder array we can leverage the fact
+        // that both arrays are the same length, so we can subtract from the current postIdx the difference
+        // of the current end of the inorder array's subsection and inorderIdx ( + 1)
         newRoot.left = buildTree(postorder, inorMap, postIdx - (inorEnd - inorderIdx  + 1), inorStart, inorderIdx - 1);
         newRoot.right = buildTree(postorder, inorMap, postIdx - 1,inorderIdx + 1, inorEnd);
 
