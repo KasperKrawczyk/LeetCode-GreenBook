bd05acc514aa5268a7abe9cbdd9c79acd9d4408a
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-19 11:35:06 +0200

    SmallestStringStartingFromLeaf

---
+++ src/SmallestStringStartingFromLeaf.java
@@ -0,0 +1,26 @@
+
+
+public class SmallestStringStartingFromLeaf {
+    public String smallestFromLeaf(TreeNode root) {
+        String[] ans = new String[] {null};
+        dfs(ans, "", root);
+        return ans[0];
+
+    }
+
+    private void dfs(String[] ans, String curPath, TreeNode root){
+        if(root == null) return;
+        char cur = (char) (root.val + 'a');
+        curPath = cur + curPath;
+        if(root.left == null && root.right == null){
+            if(ans[0] == null || ans[0].compareTo(curPath) > 0){
+                ans[0] = curPath;
+            }
+
+        } else {
+            dfs(ans, curPath, root.left);
+            dfs(ans, curPath, root.right);
+        }
+    }
+
+}
