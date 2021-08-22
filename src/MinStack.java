2774e5e5dd9a9c666313bbb926abb8b388482ac5
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-22 12:34:04 +0200

    MinStack

---
+++ src/MinStack.java
@@ -0,0 +1,36 @@
+public class MinStack {
+
+    class Node {
+        int val;
+        int min;
+        Node next;
+
+        public Node(int val, int min, Node next){
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    Node head;
+
+    public void push(int val) {
+        if(head == null){
+            head = new Node(val, val, null);
+        } else {
+            head = new Node(val, Math.min(val, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+}
