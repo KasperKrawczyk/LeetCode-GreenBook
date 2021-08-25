63e6554457d55647260e9f4575281a5ad95d5dc8
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-25 18:10:43 +0200

    SubsetsII

---
+++ src/SubsetsII.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubsetsII {
+    public List<List<Integer>> subsetsRec(int[] nums){
+        List<List<Integer>> ans = new ArrayList<>();
+        subsetsRecUtil(ans, new ArrayList<>(), nums, 0);
+        return ans;
+    }
+
+    private void subsetsRecUtil(List<List<Integer>> ans, List<Integer> temp, int[] nums, int cur){
+        if(cur == nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+
+        }
+        if(cur == 0 || nums[cur] != nums[cur - 1]){
+            temp.add(nums[cur]);
+            subsetsRecUtil(ans, temp, nums, cur + 1);
+            temp.remove(temp.size() - 1);
+        }
+
+        subsetsRecUtil(ans, temp, nums, cur + 1);
+    }
+
+}
