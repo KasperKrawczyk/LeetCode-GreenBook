ea873582aefb38d1c7d7ca7ad0c0ebc944f1ba7f
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-25 15:16:54 +0200

    Subsets

--- src/Subsets.java
+++ src/Subsets.java
@@ -32,4 +32,21 @@ public class Subsets {
         return ans;
     }
 
+    public List<List<Integer>> subsetsRec(int[] nums){
+        List<List<Integer>> ans = new ArrayList<>();
+        subsetsRecUtil(ans, new ArrayList<>(), nums, 0);
+        return ans;
+    }
+
+    private void subsetsRecUtil(List<List<Integer>> ans, List<Integer> temp, int[] nums, int cur){
+        if(cur == nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+
+        }
+        subsetsRecUtil(ans, temp, nums, cur + 1);
+        temp.remove(temp.size() - 1);
+        subsetsRecUtil(ans, temp, nums, cur + 1);
+    }
+
 }
