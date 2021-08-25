d3e0fecfbb73fc6985a7f7a2e3dcc86f32c89296
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-25 15:59:36 +0200

    InvertBinaryTree

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,14 @@
+public class InvertBinaryTree {
+    // time O(numberOfNodes), space O(heightOfTheTree)
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
