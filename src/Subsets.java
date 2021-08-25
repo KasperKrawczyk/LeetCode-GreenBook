9c0161314bbfd8e7c8d01c8918eef6527026c4de
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-25 11:31:24 +0200

    Subsets

--- src/Subsets.java
+++ src/Subsets.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Subsets {
+    // time O(n * 2^n), space O(n * 2^n)
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         backtrack(ans, new ArrayList<>(), nums, 0);
@@ -10,19 +11,11 @@ public class Subsets {
 
     private void backtrack(List<List<Integer>> ans, List<Integer> temp, int[] nums, int cur){
         ans.add(new ArrayList<>(temp));
-        System.out.println("CALLING cur = " + cur);
         for(int i = cur; i < nums.length; i++){
-            System.out.println("i = " + i);
             temp.add(nums[i]);
             backtrack(ans, temp, nums, i + 1);
-            System.out.println("remove = " + temp.get(temp.size() - 1));
             temp.remove(temp.size() - 1);
         }
-        System.out.println("TEMP WITH CUR = " + cur);
-        temp.forEach(a ->
-                System.out.print(a + " ")
-        );
-        System.out.println();
     }
 
     public List<List<Integer>> subsetsDFS(int[] nums) {
