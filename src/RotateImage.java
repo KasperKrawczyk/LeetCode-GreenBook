18a8f3824c2572e2324ce73aad352b5808607bc4
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-25 21:35:20 +0200

    RotateImage

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -1,6 +1,6 @@
 public class RotateImage {
     // time O(numOfCells), space O(1)
-    public static void rotate(int[][] matrix) {
+    public void rotate(int[][] matrix) {
         int n = matrix.length;
         for(int i = 0; i < (n + 1) / 2; i++){
             for(int j = 0; j < n / 2; j++){
@@ -9,6 +9,31 @@ public class RotateImage {
                 matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
                 matrix[j][n - 1 - i] = matrix[i][j];
                 matrix[i][j] = temp;
+            }
+        }
+    }
+
+    public void rotateTransposeAndReflect(int[][] matrix){
+        transpose(matrix);
+        reflect(matrix);
+    }
+
+    private void transpose(int[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i; j < matrix.length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    private void reflect(int[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix.length / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
             }
         }
     }
