f9aab45ff793e94b61fe3d0655c757f4c8496b2f
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-28 19:28:04 +0200

    NextGreaterElementII update: Fast stack-based solution added

--- src/NextGreaterElementII.java
+++ src/NextGreaterElementII.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class NextGreaterElementII {
 
     // time o(n^2), space O(n)
@@ -14,6 +16,27 @@ public class NextGreaterElementII {
                 }
             }
             ans[i] = curAns;
+        }
+        return ans;
+    }
+
+    // time o(n), space O(n)
+    public int[] nextGreaterElementsFast(int[] nums){
+        int n = nums.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = n - 1; i >= 0; i--){
+            stack.push(i);
+        }
+        for(int i = n - 1; i >= 0; i--){
+            ans[i] = -1;
+            while(!stack.isEmpty() && nums[stack.peek()] <= nums[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                ans[i] = nums[stack.peek()];
+            }
+            stack.push(i);
         }
         return ans;
     }
