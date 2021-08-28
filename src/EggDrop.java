57db7d81a9f9186c323748d28c9eb9bcca3b5e81
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-28 08:40:23 +0200

    SwapNodesInPairs & EggDrop update

--- src/EggDrop.java
+++ src/EggDrop.java
@@ -1,8 +1,8 @@
-import java.util.Arrays;
+
 
 public class EggDrop {
 
-    //time O(n^2e), space(ne)
+    //time O(n^2 e), space(ne)
     public static int eggDrop(int n, int e){
         int[][] dp = new int[n + 1][e + 1];
         // base case with one egg
@@ -19,15 +19,26 @@ public class EggDrop {
             for(int j = 2; j < e + 1; j++){
                 dp[i][j] = Integer.MAX_VALUE;
                 for(int x = 1; x < i; x++){
-                    int eggBroken = dp[x - 1][j - 1];
-                    int eggIntact = dp[i - x][j];
+                    int eggBroken = dp[x - 1][j - 1]; // a floor has been explored, but we have broken the egg
+                    int eggIntact = dp[i - x][j]; // a floor has been explored, and we haven't broken the egg
                     int worstCase = Math.max(eggBroken, eggIntact) + 1;
                     dp[i][j] = Math.min(dp[i][j], worstCase);
                 }
             }
         }
 
+        for(int[] row : dp){
+            for(int cell : row){
+                System.out.print("[" + cell + "]");
+            }
+            System.out.println();
+        }
+
         return dp[n][e];
+    }
+
+    public static void main(String[] args) {
+        eggDrop(1, 2);
     }
 
 
---
+++ src/SwapNodesInPairs.java
@@ -0,0 +1,81 @@
+public class SwapNodesInPairs {
+
+    // time O(n), space O(1)
+    public static ListNode swapPairs(ListNode n) {
+        if(n == null) {
+            return null;
+        }
+        if(n.next == null){
+            return n;
+        }
+        ListNode toReturn = n;
+        ListNode prev = n;
+        ListNode head = n;
+        ListNode neighbour = n;
+        ListNode next = n;
+        boolean isHead = true;
+        while(head != null && head.next != null){
+            if(isHead){
+                neighbour = head.next;
+                next = neighbour.next;
+                neighbour.next = head;
+                head.next = next;
+                toReturn = neighbour;
+                // now move the window
+                prev = head;
+                head = next;
+                isHead = false;
+                continue;
+            }
+
+            neighbour = head.next;
+            next = neighbour.next;
+            neighbour.next = head;
+            head.next = next;
+            prev.next = neighbour;
+            // now move the window
+            prev = head;
+            head = next;
+
+        }
+        return toReturn;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        ListNode l7 = new ListNode(7);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+        l6.next = l7;
+        ListNode head = swapPairs(l1);
+        while(head != null){
+            System.out.print(head.val + ", ");
+            head = head.next;
+        }
+    }
+}
