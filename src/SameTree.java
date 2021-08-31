4a66eb48b5fb3438e87d4fe91ff6eb032244aca8
KasperKrawczyk <kasper.krawczyk@gmail.com>
2021-08-31 09:26:43 +0200

    SameTree

---
+++ src/SameTree.java
@@ -0,0 +1,9 @@
+public class SameTree {
+    //time O(n), space O(log n) on a balanced tree, O(n) on an worst-case unbalanced tree
+    public boolean isSameTree(TreeNode p, TreeNode q){
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
