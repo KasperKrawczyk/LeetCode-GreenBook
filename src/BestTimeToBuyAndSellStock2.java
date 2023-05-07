034af263256b51c6a5cebe1de64dbb19bfbc3962
KasperKrawczyk <kasper.krawczyk@gmail.com>
2023-05-07 12:30:35 +0100

    added BestTimeToBuyAndSellStock2

---
+++ src/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,12 @@
+public class BestTimeToBuyAndSellStock2 {
+    // time O(n), space O(1)
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                maxProfit += (prices[i + 1] - prices[i]);
+            }
+        }
+        return maxProfit;
+    }
+}
