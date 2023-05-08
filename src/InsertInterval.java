fa5dadefdec2df2425381ca8dbc3972d004fa34d
KasperKrawczyk <kasper.krawczyk@gmail.com>
2023-05-08 17:47:53 +0100

    added InsertInterval

--- src/InsertInterval.java
+++ src/InsertInterval.java
@@ -1,52 +1,61 @@
-import java.util.Arrays;
-
 public class InsertInterval {
+    // iterative solution, O(n), time O(n)
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int len = intervals.length;
-        boolean mergeStart = false;
-        boolean mergeEnd = false;
         int start = newInterval[0];
         int end = newInterval[1];
+        int curInterval = 0;
         int insertStart = 0;
         int insertEnd = 0;
-        int[] insert;
+        int[][] ans;
         int i = 0;
-        while (start > intervals[i][0]) {
-            insertStart = i;
+        while (i < len && start > intervals[i][1]) {
             i++;
         }
-        i = 0;
+        insertStart = i;
 
-        while (end > intervals[i][1]) {
-            insertEnd = i;
+        while (i < len && end >= intervals[i][0]) {
             i++;
         }
-        // adjust
+        insertEnd = i;
 
-        if (intervals[insertStart][1] >= start) {
-            mergeStart = true;
+        ans = new int[len - insertEnd + insertStart + 1][];
+        // populate left
+        for (curInterval = 0; curInterval < insertStart; curInterval++) {
+            ans[curInterval] = new int[] {intervals[curInterval][0], intervals[curInterval][1]};
         }
 
-
-
-        if (insertEnd < len - 1 && intervals[insertEnd + 1][1] <= end) {
-            mergeEnd = true;
+        if (insertStart == insertEnd) {
+            // insert newInterval inbetween left and right
+            ans[curInterval] = newInterval;
+        } else {
+            int newInsertLeft = Math.min(newInterval[0], intervals[insertStart][0]);
+            int newInsertRight = Math.max(newInterval[1], intervals[insertEnd - 1][1]);
+            ans[curInterval] = new int[] {newInsertLeft, newInsertRight};
         }
-        if (mergeStart && mergeEnd) {
+        curInterval++;
 
+        // populate right
+        for (int j = insertEnd; j < len; j++) {
+            ans[curInterval] = new int[] {intervals[curInterval][0], intervals[curInterval][1]};
+            curInterval++;
         }
 
-        System.out.println(insertStart);
-        System.out.println(mergeStart);
-        System.out.println(insertEnd);
-        System.out.println(mergeEnd);
-
-        int[][] left = Arrays.copyOfRange(intervals, 0, insertStart);
-        int[][] right = Arrays.copyOfRange(intervals, insertEnd, len);
-
-        System.out.println(Arrays.toString(left));
-        System.out.println(Arrays.toString(right));
-        return intervals;
+        return ans;
 
     }
+
+    public static void main(String[] args) {
+        int[][] intervals = new int[][] {
+                new int[] {1, 2},
+                new int[] {3, 5},
+                new int[] {6, 7},
+                new int[] {8, 10},
+                new int[] {12, 16}
+        };
+        int[] interval = new int[] {4, 8};
+        InsertInterval ii = new InsertInterval();
+        ii.insert(intervals, interval);
+    }
+
 }
