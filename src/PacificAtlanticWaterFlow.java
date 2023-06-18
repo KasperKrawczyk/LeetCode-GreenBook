eeb9bfb5cf8371a843da40725eae25c8e348ec6b
KasperKrawczyk <kasper.krawczyk@gmail.com>
2023-06-18 18:51:06 +0100

    added PacificAtlanticWaterFlow - faster solution

--- src/PacificAtlanticWaterFlow.java
+++ src/PacificAtlanticWaterFlow.java
@@ -9,7 +9,7 @@ public class PacificAtlanticWaterFlow {
             new int[] {1, 0}   // S
     };
 
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+    public List<List<Integer>> pacificAtlanticSlow(int[][] heights) {
         List<List<Integer>> ans = new ArrayList<>();
         int height = heights.length;
         int width = heights[0].length;
@@ -60,6 +60,45 @@ public class PacificAtlanticWaterFlow {
             }
         }
         return ans;
+    }
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int height = heights.length;
+        int width = heights[0].length;
+        boolean[][] canSeePacific = new boolean[height][width];
+        boolean[][] canSeeAtlantic = new boolean[height][width];
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < height; i++) {
+            dfs(heights, canSeePacific, i, 0, Integer.MIN_VALUE);
+            dfs(heights, canSeeAtlantic, i, width - 1, Integer.MIN_VALUE);
+        }
+
+        for (int i = 0; i < width; i++) {
+            dfs(heights, canSeePacific, 0, i, Integer.MIN_VALUE);
+            dfs(heights, canSeeAtlantic, height - 1, i, Integer.MIN_VALUE);
+        }
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (canSeePacific[i][j] && canSeeAtlantic[i][j]) {
+                    ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                }
+            }
+        }
+        return ans;
+    }
+
+    private void dfs(int[][] heights, boolean[][] canSeeOcean, int y, int x, int prevHeight) {
+        if (y >= heights.length || y < 0 || x >= heights[0].length || x < 0
+                || heights[y][x] < prevHeight || canSeeOcean[y][x]) {
+            return;
+        }
+        canSeeOcean[y][x] = true;
+        dfs(heights, canSeeOcean, y, x - 1, heights[y][x]);
+        dfs(heights, canSeeOcean, y - 1, x, heights[y][x]);
+        dfs(heights, canSeeOcean, y, x + 1, heights[y][x]);
+        dfs(heights, canSeeOcean, y + 1, x, heights[y][x]);
     }
 
 }
