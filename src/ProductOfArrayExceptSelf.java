e19fe54a1687b29e7848c4a4a411bcf274674483
KasperKrawczyk <kasper.krawczyk@gmail.com>
2023-07-13 11:41:19 +0200

    updated ProductOfArrayExceptSelf

--- src/ProductOfArrayExceptSelf.java
+++ src/ProductOfArrayExceptSelf.java
@@ -2,19 +2,14 @@ public class ProductOfArrayExceptSelf {
     // time O(n), space O(n)
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
-        // for each num in nums we calculate the product of all values to its left
-        // for the first num we set it to 1, as there are no values to its left
         ans[0] = 1;
-        for(int i = 1; i < nums.length; i++){
-            ans[i] = ans[i - 1] * nums[i - 1];
+        for (int i = 1; i < ans.length; i++) {
+            ans[i] = nums[i - 1] * ans[i - 1];
         }
-        // now, for each num in nums, we calculate the product of all values to its right and multiply it with
-        // a value we already store for the num in ans[] - the product of all values to num's left
-        // here we replicate the same behaviour with a dummy variable rightRunner instead of nums[nums.length - 1] = 1
-        int rightRunner = 1;
-        for(int i = nums.length - 1; i >= 0; i--){
-            ans[i] *= rightRunner;
-            rightRunner *= nums[i];
+        int rightCumulativeProduct = 1; // the same purpose as ans[0] = 1 above
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] = rightCumulativeProduct * ans[i];
+            rightCumulativeProduct = rightCumulativeProduct * nums[i];
         }
         return ans;
     }
